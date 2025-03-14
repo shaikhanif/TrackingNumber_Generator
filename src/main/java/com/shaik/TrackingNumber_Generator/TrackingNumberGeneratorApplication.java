774b8132bec983aede6aab048331d981f39bd634
shaikhanif <shaik13787@gmail.com>
2025-03-14 12:01:45 +0530

    Project Structure committed
    Project Structure committed

---
+++ src/main/java/com/shaik/TrackingNumber_Generator/TrackingNumberGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.shaik.TrackingNumber_Generator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackingNumberGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackingNumberGeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shaik/TrackingNumber_Generator/TrackingNumberGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shaik.TrackingNumber_Generator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackingNumberGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
